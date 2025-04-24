16fd33f3d8ea33ee1bc44cf25be8083ef106b536
MATHEUS EDUARDO FREIBERGER <matheus_freiberger@estudante.sesisenai.org.br>
2025-04-24 08:54:28 -0300

    Criação das classes Guerreiro, Mago e Arqueiro herdando de Personagem

--- prova.js
+++ prova.js
@@ -7,3 +7,30 @@ class Personagem {
 }
 
 let meuPersonagem = new Personagem(100, 10, 50);
+
+class Mago extends Personagem{
+    constructor(vidaInicial, forçaInicial, manaInicial){
+        super(vidaInicial);
+        super(forçaInicial);
+        super(manaInicial);
+
+    }
+}
+
+class Arqueiro extends Personagem{
+    constructor(vidaInicial, forçaInicial, manaInicial){
+        super(vidaInicial);
+        super(forçaInicial);
+        super(manaInicial);
+
+    }
+}
+
+class Guerreiro extends Personagem{
+    constructor(vidaInicial, forçaInicial, manaInicial){
+        super(vidaInicial);
+        super(forçaInicial);
+        super(manaInicial);
+
+    }
+}
