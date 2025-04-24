3a1e786921d135cfcc900094c79b2b2e97a4a93a
MATHEUS EDUARDO FREIBERGER <matheus_freiberger@estudante.sesisenai.org.br>
2025-04-24 09:14:40 -0300

    atacar

--- prova.js
+++ prova.js
@@ -10,27 +10,30 @@ let meuPersonagem = new Personagem(100, 10, 50);
 
 class Mago extends Personagem{
     constructor(vidaInicial, forçaInicial, manaInicial){
-        super(vidaInicial);
-        super(forçaInicial);
-        super(manaInicial);
-
+        super(vidaInicial, forçaInicial, manaInicial);
     }
 }
+emitirSom() ; {
+    console.log("atacar");
+  }
 
 class Arqueiro extends Personagem{
     constructor(vidaInicial, forçaInicial, manaInicial){
-        super(vidaInicial);
-        super(forçaInicial);
-        super(manaInicial);
+        super(vidaInicial, forçaInicial, manaInicial);
 
     }
 }
+
+emitirSom() ; {
+    console.log("defender");
+  }
 
 class Guerreiro extends Personagem{
     constructor(vidaInicial, forçaInicial, manaInicial){
-        super(vidaInicial);
-        super(forçaInicial);
-        super(manaInicial);
+        super(vidaInicial, forçaInicial, manaInicial );
 
     }
 }
+emitirSom() ; {
+    console.log("usarMagia");
+  }
