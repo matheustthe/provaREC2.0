0a6eefb4c4452929281800547a872209d21b4e7b
MATHEUS EDUARDO FREIBERGER <matheus_freiberger@estudante.sesisenai.org.br>
2025-04-24 10:13:53 -0300

    Aplicação de polimorfismo no método atacar

--- prova.js
+++ prova.js
@@ -4,36 +4,49 @@ class Personagem {
     this.mana = manaInicial;
     this.força = forçaInicial;
     }
+    atacar() {
+        console.log("O personagem ataca");
+    }
 }
 
 let meuPersonagem = new Personagem(100, 10, 50);
 
-class Mago extends Personagem{
+class Guerreiro extends Personagem{
     constructor(vidaInicial, forçaInicial, manaInicial){
         super(vidaInicial, forçaInicial, manaInicial);
     }
+    espada() {
+        console.log("ataque");
+      }
+        atacar(){
+            console.log(`${this.nome} espada.`);
+        }
 }
-emitirSom() ; {
-    console.log("atacar");
-  }
 
 class Arqueiro extends Personagem{
     constructor(vidaInicial, forçaInicial, manaInicial){
         super(vidaInicial, forçaInicial, manaInicial);
-
     }
+    defesa() {
+        console.log("defender");
+      }
+        atacar(){
+            console.log(`${this.nome} flecha.`); 
+        }
 }
 
-emitirSom() ; {
-    console.log("defender");
-  }
-
-class Guerreiro extends Personagem{
+class Mago extends Personagem{
     constructor(vidaInicial, forçaInicial, manaInicial){
         super(vidaInicial, forçaInicial, manaInicial );
 
     }
+    magia() {
+        console.log("usarMagia");
+      }
+      atacar(){
+        console.log(`${this.nome} varinha.`); 
+    }
 }
-emitirSom() ; {
-    console.log("usarMagia");
-  }
+
+
+
